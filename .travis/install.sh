ef541a458bc7cbcd140e25995a8aa94e69f0a88e
Anthony Sottile <asottile@umich.edu>
2017-12-31 14:32:12 -0800

    Update deadsnakes ppa name
    The deadsnakes ppa has changed names!  For more information, see deadsnakes/issues#54
    
    Committed via https://github.com/asottile/all-repos

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-sudo add-apt-repository -y ppa:fkrull/deadsnakes
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get -y update
 
 sudo apt-get install python3.3 python3.3-dev
